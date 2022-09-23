5aec22602095be71714ad35be8903408500a22de
shafeeq.bari <shafeeq.bari@dish.com>
2022-09-23 10:28:22 -0600

    initial

---
+++ target_group.tf
@@ -0,0 +1,11 @@
+resource "aws_lb_target_group" "my_tg" {
+  name     = "mu target group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+}
+
+resource "aws_lb_target_group_attachment" "my_tg_ec2" {
+  target_group_arn = aws_lb_target_group.my_tg.arn
+  target_id        = aws_instance.my_ec2.id
+}
