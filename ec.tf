21027332d933096561032231c9605ee0bd892d35
shafeeq.bari <shafeeq.bari@dish.com>
2022-09-23 10:12:31 -0600

    initial

--- ec.tf
+++ ec.tf
@@ -1,10 +1,11 @@
 resource "aws_instance" "my_ec2" {
-  ami                    = var.ami_id
-  instance_type          = var.inst_type
-  key_name               = var.linux_key_pair
-  subnet_id              = data.aws_subnets.mypv_subnet.ids[0]
-  vpc_security_group_ids = [aws_security_group.my_ssh_80.id]
-  user_data              = file("user_data.sh")
+  ami                         = var.ami_id
+  instance_type               = var.inst_type
+  key_name                    = var.linux_key_pair
+  subnet_id                   = data.aws_subnets.mypv_subnet.ids[0]
+  vpc_security_group_ids      = [aws_security_group.my_ssh_80.id]
+  user_data                   = file("user_data.sh")
+  associate_public_ip_address = "false"
 
   root_block_device {
     volume_size           = var.my_root_volume_size
