32094a653951eb1cdb4f967b38bf79a1dba192e9
shafeeq.bari <shafeeq.bari@dish.com>
2022-09-23 17:57:10 -0600

    initial

--- ec.tf
+++ ec.tf
@@ -1,11 +1,12 @@
 resource "aws_instance" "my_ec2" {
-  ami                         = var.ami_id
-  instance_type               = var.inst_type
-  key_name                    = var.linux_key_pair
-  subnet_id                   = data.aws_subnets.mypv_subnet.ids[0]
-  vpc_security_group_ids      = [aws_security_group.my_ssh_80.id]
-  user_data_base64            = file("user_data.sh")
-  associate_public_ip_address = "false"
+  ami           = var.ami_id
+  instance_type = var.inst_type
+  key_name      = var.linux_key_pair
+  //subnet_id              = data.aws_subnets.mypv_subnet.ids[0]
+  subnet_id              = "subnet-094ebdafd1dfe224b"
+  vpc_security_group_ids = [aws_security_group.my_ssh_80.id]
+  user_data_base64       = file("user_data.sh")
+  // associate_public_ip_address = "false"
 
   root_block_device {
     volume_size           = var.my_root_volume_size
@@ -30,13 +31,14 @@ resource "aws_ami_from_instance" "my_ami" {
 }
 */
 resource "aws_instance" "my_ec2_2" {
-  ami                         = var.ami_id
-  instance_type               = var.inst_type
-  key_name                    = var.linux_key_pair
-  subnet_id                   = data.aws_subnets.mypv_subnet.ids[1]
-  vpc_security_group_ids      = [aws_security_group.my_ssh_80.id]
-  user_data_base64                   = file("user_data2.sh")
-  associate_public_ip_address = "false"
+  ami           = var.ami_id
+  instance_type = var.inst_type
+  key_name      = var.linux_key_pair
+  //subnet_id              = data.aws_subnets.mypv_subnet.ids[1]
+  subnet_id              = "subnet-0ef8ff159a6518374"
+  vpc_security_group_ids = [aws_security_group.my_ssh_80.id]
+  user_data_base64       = file("user_data2.sh")
+  // associate_public_ip_address = "false"
 
   root_block_device {
     volume_size           = var.my_root_volume_size
