a1d8aee121cf11240b2d33bc79a8bf99dc5b5d97
shafeeq.bari <shafeeq.bari@dish.com>
2022-09-23 14:50:26 -0600

    initial

--- load_bal.tf
+++ load_bal.tf
@@ -3,8 +3,8 @@ resource "aws_lb" "my_lb" {
   internal                   = false
   load_balancer_type         = "application"
   security_groups            = [aws_security_group.my_ssh_if.id]
-  subnets                    = ["subnet-07980e951c301b326", "subnet-0ef8ff159a6518374"]
-  enable_deletion_protection = true
+  subnets                    = ["subnet-07980e951c301b326", "subnet-0ef8ff159a6518374", "subnet-094ebdafd1dfe224b"]
+  enable_deletion_protection = false
 
 }
 
