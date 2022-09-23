6853fbc1d19d940aae0b2b4211868f2af029e013
shafeeq.bari <shafeeq.bari@dish.com>
2022-09-22 22:25:43 -0600

    initial

--- routes.tf
+++ routes.tf
@@ -8,4 +8,10 @@ resource "aws_route_table" "my_rt_pvt" {
   tags = {
     Name = "myprivate_rt"
   }
-} 
+}
+
+resource "aws_route_table_association" "assco_rt" {
+  for_each       = data.aws_subnets.mypv_subnet.ids
+  subnet_id      = each.value
+  route_table_id = aws_route_table.my_rt_pvt.id
+}
